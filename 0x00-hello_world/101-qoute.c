81c54d7c8269921a6dc35fb611044aa034b3fea7
MphoCM <christian.mofokeng@gmail.com>
2023-03-17 11:08:12 -0700

    11

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * maim - Entry point
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	("and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1)
+}
