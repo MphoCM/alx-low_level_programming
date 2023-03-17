6be3959367d8afe822b21c12e1f1e226a7eac931
MphoCM <christian.mofokeng@gmail.com>
2023-03-17 12:22:01 -0700

    11

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * maim - Entry point
- *
+ * main - A c program that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	("and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1)
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
